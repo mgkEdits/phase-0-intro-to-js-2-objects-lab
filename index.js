ed3bfb80f2d9d26b1116fa3ea5071887c17a72d6
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-09-14 19:20:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+let employee = {
+    name: "Elijah Nyasiando",
+    streetAddress: "Nairobi, Kenya",
+};
+  
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // Update the employee object directly
+    employee[key] = value;
+    return employee; 
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    // Create a new object without the specified key using the spread operator
+    const newEmployee = { ...employee };
+    delete newEmployee[key]; // Delete the key from the new object
+    return newEmployee; // Return the new object without the deleted key
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Delete the key from the employee object directly
+    delete employee[key];
+    return employee; // Return the updated employee
+}
+
+  
